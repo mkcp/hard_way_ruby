b5df63c27c19eec6d1c5ce88d1c9362e94ee2c80
Michael Patella <m.c.patella@gmail.com>
2013-09-22 15:12:07 -0400

    Completed LRTHW exercise

---
+++ ex1.rb
@@ -0,0 +1,12 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+puts "Here is a fancy new line!"
+
+# puts "This line is actually commented out. Also Octothorpe is a thing."
+
