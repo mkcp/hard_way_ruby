39d732515838634299ba8d94f2ab59efee4e77d7
Mike Patella <m.c.patella@gmail.com>
2013-09-25 15:30:05 -0400

    Exercise 11

---
+++ ex11.rb
@@ -0,0 +1,19 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+"Gets takes input from the command line. Chomp removes trailing whitespace."
+
+print "What's your favorite color? "
+favorite_color = gets.chomp
+print "Who is your daddy? "
+daddy = gets.chomp
+print "And what does he do? "
+daddy_occupation = gets.chomp
+
+puts "So, your favorite color is #{favorite_color}, your daddy is #{daddy} and he does #{daddy_occupation}."
