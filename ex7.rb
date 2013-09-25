cb3475ba4059a3e310fa137122444b0481d62821
Mike Patella <m.c.patella@gmail.com>
2013-09-25 14:11:17 -0400

    Exercise 7

---
+++ ex7.rb
@@ -0,0 +1,31 @@
+# printing some strings
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+
+# printing . 10 times with the multiplication operator
+puts "." * 10 # what'd that do?
+
+# declaring 12 variables each with a letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here. change it to puts
+# and see what happens.
+# concatenating all the strings together on the same line with two print()
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# this is just polite use of the terminal, try removing it
+puts
+# newline at the end for style points
